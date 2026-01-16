8c6d52c2477366974982aaf1ff6da770748285dd
bellerophon-tver <87024458+bellerophon-tver@users.noreply.github.com>
2026-01-16 19:25:44 +0300

    fix

--- hide-shots-v2.js
+++ hide-shots-v2.js
@@ -1,10 +1,21 @@
-// index.js - Lampa plugin to remove "Shots"
 (function () {
-	'use strict';
-	document.querySelectorAll('.selector').forEach(selector => {
-        const span = selector.querySelector('span.title');
-        if (span && span.textContent.trim() === 'Shots') {
-            selector.remove();
+    'use strict';
+    function startPlugin() {
+        window.hideshots = !0;
+        Lampa.Listener.follow('full', function (e) {
+            if (e.type == 'complite') {
+                setInterval(() => {
+                    ocument.querySelectorAll('.selector').forEach(selector => {
+                        const span = selector.querySelector('span.title');
+                        if (span && span.textContent.trim() === 'Shots') {
+                            selector.remove();
+                        }
+                    }
+                    }, 500);
+                }
+            })
         }
-    });
-})()
+
+        if (!window.hideshots)
+            startPlugin()
+    })()
